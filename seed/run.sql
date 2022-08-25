ddfef095f889202801906c42ddd712711e11200a
Johnson-MintyC <107607833+Johnson-MintyC@users.noreply.github.com>
2022-08-25 21:17:31 +1000

    seeded some started categories

--- seed/run.sql
+++ seed/run.sql
@@ -2,6 +2,9 @@
 
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 ALTER SEQUENCE locations_id_seq RESTART WITH 1;
+ALTER SEQUENCE categories_id_seq RESTART WITH 1;
+ALTER SEQUENCE queries_id_seq RESTART WITH 1;
 
 \i seed/users.sql
 \i seed/locations.sql
+\i seed/categories.sql
