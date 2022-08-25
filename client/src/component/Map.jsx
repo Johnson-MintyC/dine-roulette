83c07153faee3097581e1f485fd691b9548cb314
Johnson-MintyC <107607833+Johnson-MintyC@users.noreply.github.com>
2022-08-25 12:38:36 +1000

    trying leaflet

---
+++ client/src/component/Map.jsx
@@ -0,0 +1,19 @@
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+
+const Map = (props) => {
+    return (
+        <MapContainer center={position} zoom={17} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+    )
+}
